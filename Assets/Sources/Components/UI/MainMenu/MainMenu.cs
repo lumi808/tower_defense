09bc3b09a5f5b6cc5e26d6bda64f933c94174039
lumi808 <108948664+lumi808@users.noreply.github.com>
2023-03-30 19:05:17 +0600

    Improved UI and Fixed the Firegun Tower

--- Assets/Sources/Components/UI/MainMenu/MainMenu.cs
+++ Assets/Sources/Components/UI/MainMenu/MainMenu.cs
@@ -10,6 +10,8 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Button _loadGameButton;
     [SerializeField] private Button _easyGameButton;
     [SerializeField] private Button _hardGameButton;
+    [SerializeField] private Button _playGameButton;
+    [SerializeField] private Button _goBackGameButton;
     [SerializeField] private TextMeshProUGUI _easyGameText;
     [SerializeField] private TextMeshProUGUI _hardGameText;
 
@@ -20,8 +22,29 @@ public class MainMenu : MonoBehaviour
         _hardGameButton.onClick.AddListener(SetHardGame);
         _startGameButton.onClick.AddListener(StartGame);
         _loadGameButton.onClick.AddListener(LoadGame);
+        _playGameButton.onClick.AddListener(ProceedToNext);
+        _goBackGameButton.onClick.AddListener(GoBack);
     }
 
+    private void GoBack()
+    {
+        _easyGameButton.gameObject.SetActive(false);
+        _hardGameButton.gameObject.SetActive(false);
+        _startGameButton.gameObject.SetActive(false);
+        _loadGameButton.gameObject.SetActive(true);
+        _playGameButton.gameObject.SetActive(true);
+        _goBackGameButton.gameObject.SetActive(false);
+    }
+
+    private void ProceedToNext()
+    {
+        _easyGameButton.gameObject.SetActive(true);
+        _hardGameButton.gameObject.SetActive(true);
+        _startGameButton.gameObject.SetActive(true);
+        _loadGameButton.gameObject.SetActive(false);
+        _playGameButton.gameObject.SetActive(false);
+        _goBackGameButton.gameObject.SetActive(true);
+    } 
     private void LoadGame()
     {
         PlayerPrefs.SetInt(Game.LOAD_KEY, 1);
